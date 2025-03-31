c89bcb35e230720e76c0918c379d7bb2617be263
David <1392856@senati.pe>
2025-03-31 08:14:37 -0500

    Mi primer commit

---
+++ src/crud_senati/Crud_senati.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package crud_senati;
+
+/**
+ *
+ * @author Alumno-ETI
+ */
+public class Crud_senati {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
